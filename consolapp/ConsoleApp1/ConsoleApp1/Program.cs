4b78681d5358a893f4956930ba26e4ea0e82dce5
moo171 <moo130923@gmail.com>
2025-10-29 17:57:42 +0900

    10월29일 수업내용
    1.형변환에서 문자열로 바꾸는것은 Tostring() 뒤에 붙이면됩니다.
    2.Console.Write와 Console.WriteLine 차이를 이해하기
    3.Console.Read와 Console.ReadLine차이이해 4.스위치문 이해 5.반복문 while이해 6.bool 자료형 변수이해

--- consolapp/ConsoleApp1/ConsoleApp1/Program.cs
+++ consolapp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,64 @@
-﻿
-
-for (int i = 0; i <= 10; i++)
+﻿int score = 0;
+//Console.Write("입력해주세요. : ");
+bool status = true;
+Console.Write("반복할까요. : ");
+string Get_Input = Console.ReadLine();
+if (Get_Input == "네")
 {
-    if (i == 5)
+    status = true;
+}
+else
+{
+status = false;
+}
+    while (status)
     {
-        Console.WriteLine("5입니다.");
+        Console.Write("반복!!");
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine(Geting_int);
+string GetRead_String = Console.ReadLine();
+switch (GetRead_String)
+{
+    case "모자":
+        Console.Write("모자를 획득했습니다.");
+        score = 5;
+        break;
+    case "사탕":
+
+        Console.Write("사탕을 획득 했습니다!!.");
+        score = 10;
+         break;
+    default:
+         Console.Write("획득실패!");
+        score = 0;
+        break ;
 }
+        Console.WriteLine("나의 스코어는 " +score.ToString() + "입니다.");
--- consolapp/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ consolapp/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08c608c001a231c3256bb53548674aade9b7719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6585e00d68af52b892212bc810a607342582fb9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
